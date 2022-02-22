6ccc2084266f3ab6c6985692a2bb86c0614ecbf8
Ben <benjamind10@pm.me>
2022-02-22 08:49:13 -0500

    added login & authentication, closes #2

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -22,7 +22,9 @@ router.get('/:id', (req, res) => {
   })
     .then(dbUserData => {
       if (!dbUserData) {
-        res.status(404).json({ message: 'No user found with this id' });
+        res
+          .status(404)
+          .json({ message: 'No user found with this id' });
         return;
       }
       res.json(dbUserData);
@@ -47,27 +49,28 @@ router.post('/', (req, res) => {
     });
 });
 
-router.put('/:id', (req, res) => {
-  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
-
-  // pass in req.body instead to only update what's passed through
-  User.update(req.body, {
-    individualHooks: true,
+router.post('/login', (req, res) => {
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+  User.findOne({
     where: {
-      id: req.params.id,
+      email: req.body.email,
     },
-  })
-    .then(dbUserData => {
-      if (!dbUserData[0]) {
-        res.status(404).json({ message: 'No user found with this id' });
-        return;
-      }
-      res.json(dbUserData);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res
+        .status(400)
+        .json({ message: 'No user with that email address!' });
+      return;
+    }
+
+    const validPassword = dbUserData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect password!' });
+      return;
+    }
+
+    res.json({ user: dbUserData, message: 'You are now logged in!' });
+  });
 });
 
 router.delete('/:id', (req, res) => {
@@ -78,7 +81,9 @@ router.delete('/:id', (req, res) => {
   })
     .then(dbUserData => {
       if (!dbUserData) {
-        res.status(404).json({ message: 'No user found with this id' });
+        res
+          .status(404)
+          .json({ message: 'No user found with this id' });
         return;
       }
       res.json(dbUserData);
